87d418fe7d3692b735ca1f3e4e6e1e03fc333848
kirtilodha <kirtilodha0@gmail.com>
2022-02-20 16:38:25 +0530

    initial commits

---
+++ static/scripts/responses.js
@@ -0,0 +1,10 @@
+function getBotResponse(input) {
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
